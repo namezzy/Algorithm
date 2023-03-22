7895685142ee544d44dc0923e0a5b1032de0ae78
withLevi <zengyin.ssr@gmail.com>
2023-03-22 18:39:45 +0800

    add HelloArray

---
+++ src/top/withlevi/array/HelloArray.java
@@ -0,0 +1,12 @@
+package top.withlevi.array;
+
+/**
+ * Created by Levi Zhao on 3/22/2023 6:38 PM
+ *
+ * @Author Levi
+ */
+public class HelloArray {
+    public static void main(String[] args) {
+        System.out.println("Hello Array");
+    }
+}
