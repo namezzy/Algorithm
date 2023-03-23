76dfcb642a1685c8bf95db71c479396b355cc825
withLevi <zengyin.ssr@gmail.com>
2023-03-23 10:24:07 +0800

    add remote

---
+++ src/top/withlevi/array/Remote.java
@@ -0,0 +1,12 @@
+package top.withlevi.array;
+
+/**
+ * Created by Levi Zhao on 3/23/2023 10:23 AM
+ *
+ * @Author Levi
+ */
+public class Remote {
+    public static void main(String[] args) {
+        System.out.println("Add remote function");
+    }
+}
