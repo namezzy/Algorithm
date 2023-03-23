db9029a4eee58048ca0243702cd2a2fa89e7ebb9
withLevi <zengyin.ssr@gmail.com>
2023-03-23 10:32:33 +0800

    Tag

---
+++ src/top/withlevi/array/Tag.java
@@ -0,0 +1,12 @@
+package top.withlevi.array;
+
+/**
+ * Created by Levi Zhao on 3/23/2023 10:32 AM
+ *
+ * @Author Levi
+ */
+public class Tag {
+    public static void main(String[] args) {
+        System.out.println("Tag");
+    }
+}
